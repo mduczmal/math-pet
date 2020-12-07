c17245e82cfa4bc3e4c44a4575be115e3829ddd3
mduczmal <miko.duczmal@gmail.com>
2020-12-07 22:04:32 +0100

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Lottie from 'react-lottie';
+import * as animationData from './happy-unicorn-dog.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {isStopped: false, isPaused: false};
+  }
+
+  render() {
+    const buttonStyle = {
+      display: 'block',
+      margin: '10px auto'
+    };
+
+    const defaultOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: animationData.default,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+      }
+    };
+
+    return <div>
+      <Lottie options={defaultOptions}
+              height={400}
+              width={400}
+              isStopped={this.state.isStopped}
+              isPaused={this.state.isPaused}/>
+      <button style={buttonStyle} onClick={() => this.setState({isStopped: true})}>stop</button>
+      <button style={buttonStyle} onClick={() => this.setState({isStopped: false})}>play</button>
+      <button style={buttonStyle} onClick={() => this.setState({isPaused: !this.state.isPaused})}>pause</button>
     </div>
-  );
+  }
 }
-
-export default App;
