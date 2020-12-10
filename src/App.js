f6d66339d83b7d34001b57f86606e731aaa8bbe7
mduczmal <miko.duczmal@gmail.com>
2020-12-10 16:11:08 +0100

    Sweet pancake

--- src/App.js
+++ src/App.js
@@ -1,38 +1,81 @@
 import React from 'react'
 import Lottie from 'react-lottie';
-import * as animationData from './happy-unicorn-dog.json'
+import * as animationData from './happy-dog.json'
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Card"
+import {Grid, Container, Box} from "@material-ui/core";
 
 export default class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {isStopped: false, isPaused: false};
-  }
+    constructor(props) {
+        super(props);
+        this.state = {isStopped: false, isPaused: false, isFlirting: true};
+    }
 
-  render() {
-    const buttonStyle = {
-      display: 'block',
-      margin: '10px auto'
-    };
+    render() {
+        const labels = {
+            stopFlirting: "Przestań merdać!",
+            startFlirting: "Zacznij merdać!"
+        }
+        /*const buttonStyle = {
+        display: 'block',
+        margin: '10px auto'
+      };*/
+        const paper = {
+            textAlign: 'center',
+        }
+        const button = {
+            minWidth: 200,
+        }
 
-    const defaultOptions = {
-      loop: true,
-      autoplay: true,
-      animationData: animationData.default,
-      rendererSettings: {
-        preserveAspectRatio: 'xMidYMid slice'
-      }
-    };
+        const defaultOptions = {
+            loop: 0.5,
+            autoplay: true,
+            animationData: animationData.default,
+            rendererSettings: {
+                preserveAspectRatio: 'xMidYMid slice'
+            },
+        };
 
-    return <div>
-      <Lottie options={defaultOptions}
-              height={400}
-              width={400}
-              isStopped={this.state.isStopped}
-              isPaused={this.state.isPaused}/>
-      <button style={buttonStyle} onClick={() => this.setState({isStopped: true})}>stop</button>
-      <button style={buttonStyle} onClick={() => this.setState({isStopped: false})}>play</button>
-      <button style={buttonStyle} onClick={() => this.setState({isPaused: !this.state.isPaused})}>pause</button>
-    </div>
-  }
+        return <div>
+            <Container>
+                <Box mt={3} mx={'auto'}>
+                    <Grid container spacing={2} justify={"center"}>
+                        <Paper className={paper}>
+                            <Grid px="30" container alignItems={'center'}>
+                                <Grid item xs={12}>
+                                    <Lottie options={defaultOptions}
+                                            eventListeners={
+                                                [{
+                                                    eventName: "complete",
+                                                    callback: () => {
+                                                        this.setState({isStopped: true});
+                                                        setTimeout(() => this.setState({isStopped:false}), 1000)
+                                                    }
+                                                }]}
+                                            height={400}
+                                            width={400}
+                                            isClickToPauseDisabled={true}
+                                            isStopped={this.state.isStopped}
+                                            isPaused={this.state.isPaused}/>
+                                </Grid>
+                                <Grid container item xs={12} justify={'center'}>
+                                    <Box width={300}>
+                                        <Grid container justify={'center'}>
+                                        <Button classes={button} onClick={() => this.setState({
+                                            isPaused: !this.state.isPaused,
+                                            isFlirting: !this.state.isFlirting
+                                        })}>
+                                            {this.state.isFlirting? labels.stopFlirting : labels.startFlirting}
+                                        </Button>
+                                        </Grid>
+                                    </Box>
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                    </Grid>
+                </Box>
+            </Container>
+        </div>
+    }
 }
